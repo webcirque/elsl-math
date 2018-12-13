3c7a6136fccdec7ab5f9253dea7468df5f093f8d
Sky Fu (Chdkfd) <SkyFuInMC@users.noreply.github.com>
2018-12-13 12:46:09 +0800

    Minor update

--- geometric.js
+++ geometric.js
@@ -30,21 +30,27 @@ function ComplexNumber(r = 0, i = 0) {
         };
     };
     this.divide = function (object) {
-        
+        //(a+bi)/(c+di)=(ac+bd)/(c2+d2) +((bc-ad)/(c2+d2))i
+        if (object.constructor == Number) {
+            return new ComplexNumber(this.realPart / object, this.imaginaryPart / object);
+        } else {
+            return new ComplexNumber((this.realPart * object.realPart + this.imaginaryPart * object.imaginaryPart) / (object.realPart ** 2 + object.imaginaryPart ** 2), (this.imaginaryPart * object.realPart - this.realPart * object.imaginaryPart) / (object.realPart ** 2 + object.imaginaryPart ** 2));
+        };
     };
 }
-function Vector() {
-    this.position = [];
-    this.setPosition = function (...pos) {
+function GeoVector() {
+    this.extension = [];
+    this.start = [];
+    this.setCoordinates = function (...pos) {
         pos.forEach((e, i) => {
            if (e.constructor != Number && e.constructor != ComplexNumber)  {
                throw(new TypeError("Invalid position announce at index $1.".replace("$1", i)));
            }
         });
-        this.position = pos;
+        this.extension = pos;
         return this;
     };
-    this.set2dRadicalPos = function () {
+    this.set2dPolarCoordinates = function () {
         
     };
     this.plus = function () {
